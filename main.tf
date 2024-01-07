f6fce0f1e82faa125e2a35ea5bdd5fdf3263b85a
Nitin Khaitan <khaitan.nitin@gmail.com>
2024-01-07 18:45:37 +1100

    Initial configuration for AFT | Cost Optimised

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 module "aft" {
-  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  source                      = "github.com/khaitan-nitin/terraform-aws-control_tower_account_factory"
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
   audit_account_id            = var.audit_account_id
@@ -15,4 +15,10 @@ module "aft" {
   account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
   global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
   account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+
+  cloudwatch_log_group_retention          = var.cloudwatch_log_group_retention
+  aft_feature_disable_private_networking  = var.aft_feature_disable_private_networking
+  aft_vpc_endpoints                       = var.aft_vpc_endpoints
+  aft_feature_cloudtrail_data_events      = var.aft_feature_cloudtrail_data_events
+  aft_feature_delete_default_vpcs_enabled = var.aft_feature_delete_default_vpcs_enabled
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,16 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-ct_management_account_id    = ""
-log_archive_account_id      = ""
-audit_account_id            = ""
-aft_management_account_id   = ""
-github_username             = ""
-ct_home_region              = ""
+ct_management_account_id    = "923393413720"
+log_archive_account_id      = "163563420026"
+audit_account_id            = "432630990765"
+aft_management_account_id   = "767397998320"
+github_username             = "khaitan-nitin"
+ct_home_region              = "ap-southeast-2"
 tf_backend_secondary_region = ""
+
+cloudwatch_log_group_retention          = "1"
+aft_feature_disable_private_networking  = true
+aft_feature_cloudtrail_data_events      = false
+aft_vpc_endpoints                       = false
+aft_feature_delete_default_vpcs_enabled = true
